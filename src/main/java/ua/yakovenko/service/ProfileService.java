10251d40b96914f8dd87c4bd0becec2becbf7904
Alexandr Yakovenko <yakovenko9814@ukr.net>
2019-08-29 13:20:16 +0300

    add button edit profile

---
+++ src/main/java/ua/yakovenko/service/ProfileService.java
@@ -0,0 +1,31 @@
+package ua.yakovenko.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import ua.yakovenko.domain.User;
+import ua.yakovenko.repository.UserRepository;
+
+@Service
+public class ProfileService {
+    @Autowired
+    private UserRepository userRepository;
+
+    //TODO сделать так чтоб нельзя было менять Ник не подтвердив пароль
+    public void updateProfile(User user, String username, String password) {
+        String oldUsername = user.getUsername();
+
+        boolean isUsernameChanged = (username != null && !username.equals(oldUsername))
+                || (oldUsername != null && !oldUsername.equals(username));
+
+        if (isUsernameChanged) {
+            user.setUsername(username);
+        }
+
+        if (!StringUtils.isEmpty(password)) {
+            user.setPassword(password);
+        }
+
+        userRepository.save(user);
+    }
+}
